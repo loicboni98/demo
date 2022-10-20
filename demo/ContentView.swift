f0766b82310e4872b91632f7c639230b28c45573
loicboni <loic.boni@yahoo.com>
2022-10-20 10:42:13 +0200

    Initial Commit

---
+++ demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo
+//
+//  Created by apprenant68 on 20/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by apprenant68 on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
